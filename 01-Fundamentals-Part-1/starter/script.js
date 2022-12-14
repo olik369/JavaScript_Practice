83d3d9e075375b9cbdc99aa3c2e1e8786a89b08b
HAHAHA <olik369@gmail.com>
2022-12-15 01:17:23 +0900

    Section2 : Coding Challenge #2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -231,8 +231,52 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 
-// 19장 Coding Challenge #2
+//* 19장 Coding Challenge #2
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement
+*/
 
+//* CODE
+/*
+console.log('Data 1');
+let markMassKg = 78;
+let markHeightMeter = 1.69;
+let markBMI = markMassKg / markHeightMeter ** 2;
+
+let johnMassKg = 92
+let johnHeightMeter = 1.95
+let johnBMI = johnMassKg / johnHeightMeter ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
+
+console.log('Data 2');
+markMassKg = 95;
+markHeightMeter = 1.88
+markBMI = markMassKg / markHeightMeter ** 2;
+
+johnMassKg = 85;
+johnHeightMeter = 1.76;
+johnBMI = johnMassKg / johnHeightMeter ** 2;
+
+markHigherBMI = markBMI > johnBMI;
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
+*/
 
 // 20장 Type Conversion and Coercion
 // 21장 Truthy and Falsy Values

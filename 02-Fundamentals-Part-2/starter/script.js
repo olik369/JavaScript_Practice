6d53a5d13467f7d118fadd0c5f7b5acd3d1ccc74
HAHAHA <olik369@gmail.com>
2022-12-21 00:17:36 +0900

    34장까지

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,7 +8,6 @@
 !1. 특정 작업을 금지함
 !2. 눈에 보이는 오류를 발생시킴
 */
-
 //* CODE
 /*
 let hasDriversLicense = false;
@@ -32,7 +31,6 @@ function 함수이름 (매개변수) {
 
 함수에 반드시 매개변수와 리턴값이 존재할 필요는 없음!
 */
-
 //* CODE
 /*
 function logger() {
@@ -60,7 +58,41 @@ console.log(appleOrangeJuice);
 */
 
 //* 34장 Function Declarations vs. Expressions
+/*
+! 서로 같은 의미가 아님!!
+매개변수(parameters) : 함수에서 사용하는 변수
+인수(arguments) : 함수를 호출하는 곳에서 사용하는 실제 값
 
+!일반적인 함수 선언 VS 익명 함수 선언
+일반적인 함수 선언을 통해 함수를 사용할 시에는 먼저 함수를 정의할 필요가 없음!!
+ex)
+변수 = 함수호출
+일반적인 함수선언
+(문제 없음!!)
+
+변수 = 함수호출
+익명함수 선언
+(에러 발생!!)
+?==> 이러한 차이는 내부에서 호이스팅(hoisting)이라는 과정때문에 차이를 보이는 것임
+익명함수 선언은 커링을 사용할때나 내부함수를 사용할때 매우 유용하게 쓰일것임
+*/
+//* CODE
+/*
+일반적인 함수 선언(함수 선언 방법1)
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+함수 호출!
+const age1 = calcAge1(1991);
+
+익명 함수(함수 선언 방법2)
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+*/
 
 //* 35장 Arrow Functions
 
